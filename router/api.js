125b627d3d0ff48e1404e1152028f7cab3d9b6b7
NGO HOANG GIANG <142860987+giang196shuket@users.noreply.github.com>
2024-03-07 08:05:27 +0700

    git commit -m "add filename to .gitignore"

--- router/api.js
+++
@@ -1,7 +0,0 @@
-var express = require("express");
-var router = express.Router();
-const homeController = require("../app/controller/Home");
-
-router.post("/home/getHospitalList", homeController.api_getHospitalList);
-
-module.exports = router;
